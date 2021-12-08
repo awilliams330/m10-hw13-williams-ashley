e55a6fa353d4e4516db721f400c6672d20262501
Ashley Williams <awilliams330@ufl.edu>
2021-12-08 12:07:22 -0500

    Fix const destructuring section

--- index.js
+++ index.js
@@ -32,15 +32,15 @@ async function getWeather(query) {
     query +
     '&units=imperial&appid=6efff70fe1477748e31c17d1c504635f'
   );
-  const {data: {cod, coord, dt, main, name, sys, weather} } = await res.json();
+  const {cod, coord, dt, main, name, sys, weather} = await res.json();
   // location not found, throw error/reject promise
   if (cod === "404")
     throw new Error('location not found');
   // create weather icon URL
   var iconUrl = 'https://openweathermap.org/img/wn/' +
-    weather.icon +
+    weather[0].icon +
     '@2x.png';
-  var description = weather.description;
+  var description = weather[0].description;
   var actualTemp = main.temp;
   var feelsLikeTemp = main.feels_like;
   var place = `${name}, ${sys.country}`;
